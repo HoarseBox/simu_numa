6d983b17c6eb6687a753e7b3608ee1f8c33bcdc7
Shijie Xu <shijiexu@eecs583a.eecs.umich.edu>
2017-12-04 16:56:25 -0500

    find thread -> core num map

--- lib/tlayout/tlayout.cpp
+++ lib/tlayout/tlayout.cpp
@@ -17,6 +17,7 @@ namespace {
 
     static const StringRef FUNCNAME_PTHREAD_CREATE;
     static const StringRef FUNCNAME_PTHREAD_ATTR_SETAFFINITY_NP;
+    static const unsigned int NUM_CORES;
 
     static char ID;
     Tlayout() : ModulePass(ID) {}
@@ -24,12 +25,17 @@ namespace {
     bool runOnModule(Module &M);
 
   private:
-    DenseMap<CallInst*, Instruction*> Thread2AffinityMap; // thread_create Inst -> setaffinity Inst
-    // std::set<StringRef> ThreadFuncNameSet;
+    DenseMap<CallInst*, CallInst*> Thread2AffinityMap; // thread_create Inst -> setaffinity Inst
+
+    DenseMap<CallInst*, Instruction*> Thread2CPUSetInstMap;
 
     DenseMap<Instruction*, std::set<Instruction*> > ThreadGlobalDataMap;
 
     DenseMap<Instruction*, std::set<CallInst*> > Data2Threads;
+
+    DenseMap<CallInst*, int> Thread2NewCoreNum;
+
+    //DenseMap<int, std::set<CallInst*> > coreNum2Threads;
 
 
     void createThreadInfoRecord(Module &M, const bool DEBUG = false);
@@ -42,6 +48,7 @@ namespace {
 
 const StringRef Tlayout::FUNCNAME_PTHREAD_CREATE = StringRef("pthread_create");
 const StringRef Tlayout::FUNCNAME_PTHREAD_ATTR_SETAFFINITY_NP = StringRef("pthread_attr_setaffinity_np");
+const unsigned int Tlayout::NUM_CORES = 8;
 
 }  // end of anonymous namespace
 
@@ -107,8 +114,43 @@ void Tlayout::createThreadInfoRecord(Module &M, const bool DEBUG) {
                 errs() << "ERROR: didn't set affinity before create thread!\n";
                 break; 
               }
-              Instruction* setAffinityInst = dyn_cast<Instruction>(*UI);
+              //errs() << "\t\t"<< dyn_cast<Value>(*UI)->getArgOperand(2) << '\n';
+
+              // Instruction* setAffinityInst = dyn_cast<Instruction>(*UI);
+              CallInst* setAffinityInst = dyn_cast<CallInst>(*UI);
+              errs() << "\t\t"<< *(setAffinityInst->getArgOperand(2)) << '\n';
+              //BasicBlock* pBB = setAffinityInst->getParent();
+              LLVMBasicBlockRef currentBBRef = wrap(setAffinityInst->getParent());
+              LLVMBasicBlockRef setCoreBBRef;
+              for (int i = 0; i < 3; ++i){
+                setCoreBBRef = LLVMGetPreviousBasicBlock(currentBBRef);
+                currentBBRef = setCoreBBRef;
+              }
+              BasicBlock* setCoreBB = unwrap(setCoreBBRef);
+              for (BasicBlock::iterator I = setCoreBB->begin(); I != setCoreBB->end(); ++I){
+                Instruction* temp = dyn_cast<Instruction>(I);
+                if (I && isa<StoreInst>(temp)){
+                  //errs() << *I << '\n';
+                  //errs() << I->getNumOperands() << '\n';
+                  Thread2CPUSetInstMap[callInst] = temp;
+                }
+              }
+              //errs() << *(unwrap(setCoreBBRef)) << '\n';
+              /*
+              errs() << *pBB << '\n';
+              LLVMBasicBlockRef previousBBRef = LLVMGetPreviousBasicBlock(wrap(pBB));
+              errs() << *(unwrap(previousBBRef)) << '\n';
+              LLVMBasicBlockRef ppBBRef = LLVMGetPreviousBasicBlock(previousBBRef);
+              errs() << *(unwrap(ppBBRef)) << '\n';
+              LLVMBasicBlockRef pppBBRef = LLVMGetPreviousBasicBlock(ppBBRef);
+              errs() << *(unwrap(pppBBRef)) << '\n';
+              */
+              //Instruction* setCPUInst = dyn_cast<Instruction>(setAffinityInst->getArgOperand(2));
+              //errs() << "\t\tset CPU operand "<< *(setCPUInst->getOperand(0)) << '\n';
+
               Thread2AffinityMap[callInst] = setAffinityInst;
+              
+              //Thread2CPUMap[callInst] = 
               if (DEBUG) errs() << "\t\t\t" << *setAffinityInst << '\n';
               break;
             }
@@ -146,8 +188,14 @@ void Tlayout::dfsRootAncestor(Instruction* threadInst, Instruction* child, const
 */
 
 void Tlayout::createGlobalDataRecord(Module &M, const bool DEBUG) {
+  // TODO: serialized threads
 
-  for (DenseMap<CallInst*, Instruction*>::iterator MI = Thread2AffinityMap.begin();
+
+  // TODO: parallelized threads
+  // find mutex, if two thread functions acquire the same mutex, they use the shared data
+
+
+  for (DenseMap<CallInst*, CallInst*>::iterator MI = Thread2AffinityMap.begin();
     MI != Thread2AffinityMap.end(); ++MI) {
 
     CallInst *threadCreateInst = MI->first;
@@ -159,10 +207,15 @@ void Tlayout::createGlobalDataRecord(Module &M, const bool DEBUG) {
     Value *arg = threadCreateInst->getArgOperand(3);
     errs() << arg << '\n';
     Instruction* argInst = dyn_cast<Instruction>(arg);
+    /*
     errs() << "argInst\t\t" << *argInst << '\n';
 
     Instruction* originalData = dyn_cast<Instruction>(argInst->getOperand(0));
     Data2Threads[originalData].insert(threadCreateInst);
+    */
+
+
+
 
 
     //find ancestor 
@@ -192,10 +245,33 @@ void Tlayout::createGlobalDataRecord(Module &M, const bool DEBUG) {
 
 
 void Tlayout::findDataOverlapping(const bool DEBUG) {
+  unsigned int currentCore = 0;
+  for (DenseMap<Instruction*, std::set<CallInst*> >::iterator MI = Data2Threads.begin();
+    MI != Data2Threads.end(); ++MI){
+    std::set<CallInst*> accessCommonDataThreads = MI->second;
+    currentCore %= NUM_CORES;
+    //coreNum2Threads[currentCore] = accessCommonDataThreads;
+    for (std::set<CallInst*>::iterator SI = accessCommonDataThreads.begin(); SI != accessCommonDataThreads.end(); ++SI){
+      CallInst* threadCreateInst = dyn_cast<CallInst>(*SI);
+      Thread2NewCoreNum[threadCreateInst] = currentCore;
+    }
+    currentCore++;
+  }
+
+
   return;
 }
 
 void Tlayout::optimizeThreadLocation(const bool DEBUG) {
+  for (DenseMap<CallInst*, CallInst*>::iterator MI = Thread2AffinityMap.begin();
+    MI != Thread2AffinityMap.end(); ++MI){
+    CallInst* threadCreateInst = dyn_cast<CallInst>(MI->first);
+    Instruction* setCoreInst = Thread2CPUSetInstMap[threadCreateInst];
+    int coreNum = Thread2NewCoreNum[threadCreateInst];
+    errs() << coreNum << '\n';
+    errs() << *threadCreateInst << '\n';
+  } 
   return;
 }
+
 
