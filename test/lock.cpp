1126efbd3f33be87828d862ad041d6e935020ff8
Yuxuan Zhang <zyuxuan@eecs583a.eecs.umich.edu>
2017-12-04 14:46:22 -0500

    modified:   lock.cpp 	modified:   ../Makefile.common

--- test/lock.cpp
+++ test/lock.cpp
@@ -40,7 +40,7 @@ int main(int argc, char** argv){
 	int coreNum2=1;
 
 	// threads that we want to map	
-	int NumThreads = 2;
+	int NumThreads = 1000;
 	pthread_t threads[NumThreads];
 	pthread_attr_t attr;
 	cpu_set_t cpus;
@@ -65,18 +65,37 @@ int main(int argc, char** argv){
 		p[i].addr2 = tmp2;
 		p[i].ID = i;
 	}
+
+	auto start = chrono::high_resolution_clock::now();
 	
 	for (int i=0; i<NumThreads; i++){
 		pthread_attr_init(&attr);
 		CPU_ZERO(&cpus);
 		CPU_SET(i, &cpus);
 		int threadNum = i;
+		if (i%2==0){
+			pthread_attr_init(&attr);
+			CPU_ZERO(&cpus);
+			CPU_SET(i%8, &cpus);
+		}
+		else{
+			pthread_attr_init(&attr);
+			CPU_ZERO(&cpus);
+			CPU_SET(i%8+8, &cpus);
+		}
 		pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
 		pthread_create(&threads[i], &attr, DoWork, (void*)&p[i]);
+
 	}
 	for (int i=0; i<NumThreads; i++){
 		pthread_join(threads[i], NULL);
 	}
+
+	auto end = chrono::high_resolution_clock::now();
+	std::chrono::duration<double> diff = end - start;
+	cout<<"It took me "<<diff.count()<<"seconds."<<endl;
+
+
 //	auto start = chrono::high_resolution_clock::now();
 //	for (int i=0; i<NumThreads; i++){
 //		pthread_attr_init(&attr);
