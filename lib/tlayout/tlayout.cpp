46d36103b22944bc509e2450eef3927bc6bf6a0c
Yuxuan Zhang <zyuxuan@eecs583a.eecs.umich.edu>
2017-12-09 11:16:25 -0500

    Changes to be committed:

--- lib/tlayout/tlayout.cpp
+++ lib/tlayout/tlayout.cpp
@@ -203,52 +203,7 @@ void Tlayout::createThreadInfoRecord(Module &M, const bool DEBUG) {
 	  	} 
         }
 
- /*       // Find the CallInst
-        Instruction &I = *II;
-        if (!isa<CallInst>(I)) {
-          continue;
-        }
-        CallInst *callInst = dyn_cast<CallInst>(&I);
-        Function *calledFunc = callInst->getCalledFunction();
-        if (!calledFunc) {
-          errs() << "\tWARN: Indirect function call.\n";
-          continue;
-        }
 
-        // Find function call of pthread_create and pthread_attr_setaffinity_np
-        StringRef funcName = calledFunc->getName();
-        if (funcName.equals(FUNCNAME_PTHREAD_CREATE)) {
-          if (DEBUG) errs() << "\t";
-          if (DEBUG) errs().write_escaped(funcName) << '\n';
-
-          // Find the function called by a thread
-          Value *threadAttr = callInst->getArgOperand(1);
-          for (Value::use_iterator UI = threadAttr->use_begin(), E = threadAttr->use_end(); UI != E; ++UI){
-            Instruction* user = dyn_cast<Instruction>(*UI);
-            if (DEBUG) errs() << "\t\t" << *user << '\n';
-            if (user == callInst) {
-              UI++;
-              if (UI == E) {
-                errs() << "ERROR: didn't set affinity before create thread!\n";
-                break; 
-              }
-              Instruction* setAffinityInst = dyn_cast<Instruction>(*UI);
-              Thread2AffinityMap[callInst] = setAffinityInst;
-              if (DEBUG) errs() << "\t\t\t" << *setAffinityInst << '\n';
-              break;
-            }
-          }
-
-          // StringRef threadFuncName = callInst->getArgOperand(2)->getName();
-          // ThreadFuncNameSet.insert(threadFuncName);
-
-
-        } else if (funcName.equals(FUNCNAME_PTHREAD_ATTR_SETAFFINITY_NP)) {
-          if (DEBUG) errs() << "\t";
-          if (DEBUG) errs().write_escaped(funcName) << '\n';
-
-        }
-*/        
       } //end for Instr
 
 
